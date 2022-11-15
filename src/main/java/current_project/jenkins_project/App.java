10ebbd5e9648c9a6a6a65934ec48809c73b46b7b
hemanth karuparthi <karuparthihemanth@gmail.com>
2022-11-15 14:21:06 +0530

    myfirstcommit

---
+++ src/main/java/current_project/jenkins_project/App.java
@@ -0,0 +1,13 @@
+package current_project.jenkins_project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/current_project/jenkins_project/AppTest.java
@@ -0,0 +1,20 @@
+package current_project.jenkins_project;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
